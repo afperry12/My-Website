ce972f1535df6392126b71206bc19dfae44b63aa
Peter Perry <afperry@dons.usfca.edu>
2023-03-07 15:47:21 -0800

    Update Space Agents

--- routes/spaceagents.py
+++ routes/spaceagents.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('spaceagents', __name__)
 
-@bp.route('/')
+@bp.route('/spaceagents')
 def home():
     return render_template('spaceagents.html')
